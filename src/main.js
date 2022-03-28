eeb73bba0f232da4acfb3b40fb3bd15574a4238f
Yotam <190710171@uajy.ac.id>
2022-03-28 15:37:55 +0700

    gfirs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 
-Vue.config.productionTip = false
+import vuetify from "@/plugins/vuetify";
+import router from './route'
+// import axios from 'axios';
+
+Vue.config.productionTip = false;
+
+// Vue.prototype.$http = axios;
+// Vue.prototype.$api = 'http://127.0.0.1:8000/api'; //link backend
+
 
 new Vue({
+  vuetify,
   router,
   render: h => h(App)
 }).$mount('#app')
